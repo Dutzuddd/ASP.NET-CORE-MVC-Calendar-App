1f36b3d8a700f5352348e78ff05d01449785aeb4
Dutzuddd <vladsfranciog@gmail.com>
2023-04-26 22:13:14 +0300

    initial Commit

---
+++ Proiect_1_test/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect_1_test.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public virtual Location Location { get; set; }
+        public virtual ApplicationUsercs User { get; set; }
+    }
+}
